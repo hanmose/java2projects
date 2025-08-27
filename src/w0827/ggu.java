22001565135a879198f2b76bbe259f9dbf42e597
hanmose <nightsound5124@gmail.com>
2025-08-27 09:23:39 +0900

    구구단

---
+++ src/w0827/ggu.java
@@ -0,0 +1,14 @@
+package w0827;
+
+public class ggu {
+    public static void main(String[] args) {
+        int one,two;
+
+        for (one = 2; one <=9; one++){
+            for (two = 1; two <= 9; two++){
+                System.out.printf("%d * %d = %2d ", one,two,one*two);
+            }
+            System.out.println("");
+        }
+    }
+}
