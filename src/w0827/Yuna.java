21e904ac978008249a368a89739a5b241219b588
hanmose <nightsound5124@gmail.com>
2025-08-27 11:47:01 +0900

    스케이팅 점수

---
+++ src/w0827/Yuna.java
@@ -0,0 +1,26 @@
+package w0827;
+
+import java.util.Scanner;
+
+public class Yuna {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.printf("점수 %d : 10\n",i);
+        }
+        System.out.println("평균 점수 : 10");
+        //10점이 아닐리가 없음
+
+//        Scanner sc = new Scanner(System.in);
+//        int[] scores = new int[5];
+//        int sum = 0;
+//        double avg = 0;
+//
+//        for (int i = 0; i < scores.length; i++) {
+//            System.out.print("점수 "+(i+1)+ " 입력");
+//            scores[i] = sc.nextInt();
+//            sum += scores[i];
+//        }
+//        avg = sum / scores.length;
+//        System.out.println("평균 점수"+avg);
+    }
+}
