a86e7fafd289b9c2180a4e01549d9190aab804fc
hanmose <nightsound5124@gmail.com>
2025-08-27 10:43:31 +0900

    Array 역순 변경

---
+++ src/w0827/ArrayReverseTest.java
@@ -0,0 +1,16 @@
+package w0827;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class ArrayReverseTest {
+    public static void main(String[] args) {
+        String[] strArr = {"월","화","수","목","금","토","일"};
+        System.out.println("처음 배열"+ Arrays.toString(strArr));
+
+        Collections.reverse(Arrays.asList(strArr));
+
+        System.out.println("역순 배열"+ Arrays.toString(strArr));
+    }
+}
