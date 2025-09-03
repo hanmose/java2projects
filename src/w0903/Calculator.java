14ac9c4a7edf92e87d7eddf3b48de94455103ec4
hanmose <nightsound5124@gmail.com>
2025-09-03 11:39:53 +0900

    단순 계산기

--- src/w0903/Calculator.java
+++ src/w0903/Calculator.java
@@ -22,24 +22,25 @@ public class Calculator {
 
     }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         int one = 0;
         int two = 0;
         char symbol = 0;
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
         System.out.println("정수1 입력: ");
         one = sc.nextInt();
         System.out.println("정수2 입력: ");
         two = sc.nextInt();
         System.out.println("연산기호 입력(e입력시 종료: ");
         symbol = sc.next().charAt(0);
-        if(symbol=='e') {
+        if(symbol=='e'){
             System.out.println("종료");
             return;
         }
-
-
-
         System.out.println(Calc(one, two, symbol));
+        }
+
 
     }
 }
