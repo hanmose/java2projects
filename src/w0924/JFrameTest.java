b09f50a208eb2513e9db49a1286dcea432a0b2fb
hanmose <nightsound5124@gmail.com>
2025-09-24 11:46:06 +0900

    GUI

---
+++ src/w0924/JFrameTest.java
@@ -0,0 +1,12 @@
+package w0924;
+
+public class JFrameTest extends javax.swing.JFrame {
+    JFrameTest() {
+        setTitle("처음 만드는 프레임");
+        setSize(500, 500);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new JFrameTest();
+    }
+}
